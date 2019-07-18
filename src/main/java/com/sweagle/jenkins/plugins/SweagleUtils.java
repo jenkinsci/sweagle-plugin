d5d40843101c4a24c4857d73ace27673dd2e4e6e
joeo <joeo@192.168.138.180>
2019-07-18 17:28:52 -0400

    Updated readme to match wiki page.

--- src/main/java/com/sweagle/jenkins/plugins/SweagleUtils.java
+++ src/main/java/com/sweagle/jenkins/plugins/SweagleUtils.java
@@ -35,15 +35,13 @@ public class SweagleUtils {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SweagleUtils.class);
 
 	static String validateConfig(String mdsName, String sweagleURL, Secret sweagleAPIkey, boolean markFailed,
-			int warnMax, int errMax, TaskListener listener, boolean showResults, Run<?, ?> run) throws InterruptedException, AbortException {
+			int warnMax, int errMax, TaskListener listener, boolean showResults, Run<?, ?> run)
+			throws InterruptedException, AbortException {
 
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
-		OkHttpClient client = new OkHttpClient().newBuilder()
-		.connectTimeout(60, TimeUnit.SECONDS)
-        .writeTimeout(60, TimeUnit.SECONDS)
-        .readTimeout(60, TimeUnit.SECONDS)
-        .build();
+		OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(60, TimeUnit.SECONDS)
+				.writeTimeout(60, TimeUnit.SECONDS).readTimeout(60, TimeUnit.SECONDS).build();
 		Response response = null;
 		loggerUtils.info("Checking MDS Validity: " + mdsName);
 
@@ -65,49 +63,49 @@ public class SweagleUtils {
 
 		int mdsErrors = JsonPath.read(responseString, "summary.errors");
 		int mdsWarns = JsonPath.read(responseString, "summary.warnings");
-		loggerUtils.info(mdsName + " contains " + mdsWarns + " warnings and "+mdsErrors+" errors");
+		loggerUtils.info(mdsName + " contains " + mdsWarns + " warnings and " + mdsErrors + " errors");
 		if (mdsErrors > errMax & errMax != -1) {
 			if (showResults)
 				loggerUtils.debug(responseString);
 			if (markFailed)
-			throw new AbortException(" Errors: " + mdsErrors + " Exceeds error threshold: " + errMax);
+				throw new AbortException(" Errors: " + mdsErrors + " Exceeds error threshold: " + errMax);
 		}
 		if (mdsWarns > warnMax & warnMax != -1) {
 			if (showResults)
 				loggerUtils.debug(responseString);
 			if (markFailed)
-			throw new AbortException(" Warnings: " + mdsWarns + " Exceeds warning threshold: " + warnMax);
+				throw new AbortException(" Warnings: " + mdsWarns + " Exceeds warning threshold: " + warnMax);
 		}
 		return responseString;
 
 	}
 
-	static String uploadConfig(String sweagleURL, Secret sweagleAPIkey, String fileLocation, String nodePath, String format, boolean markFailed, TaskListener listener, EnvVars env ) throws AbortException {
+	static String uploadConfig(String sweagleURL, Secret sweagleAPIkey, String fileLocation, String nodePath,
+			String format, boolean markFailed, TaskListener listener, EnvVars env) throws AbortException {
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
 		loggerUtils.info("Uploading Config from " + fileLocation + " to " + nodePath);
 		String responseString = null;
 		String content = null;
 		try {
-			content = readFile(env.get("WORKSPACE")+"/"+fileLocation, Charset.defaultCharset());
+			content = readFile(env.get("WORKSPACE") + "/" + fileLocation, Charset.defaultCharset());
 		} catch (IOException e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
-			else 
+			else
 				loggerUtils.error(e.toString());
 		}
 		OkHttpClient client = new OkHttpClient();
-		
+
 		MediaType mediaType = MediaType.parse("text/plain");
 		RequestBody body = RequestBody.create(mediaType, content);
 		Request request = new Request.Builder()
-		  .url(sweagleURL +"/api/v1/data/bulk-operations/dataLoader/upload?nodePath="+nodePath+"&format="+format+"&allowDelete=false&autoApprove=true&storeSnapshotResults=false&validationLevel=error")
-		  .post(body)
-		  .addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
-		  .addHeader("Accept", "*/*")
-		  .addHeader("Cache-Control", "no-cache")
-		  .addHeader("Connection", "keep-alive")
-		  .build();
+				.url(sweagleURL + "/api/v1/data/bulk-operations/dataLoader/upload?nodePath=" + nodePath + "&format="
+						+ format
+						+ "&allowDelete=false&autoApprove=true&storeSnapshotResults=false&validationLevel=error")
+				.post(body).addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
+				.addHeader("Accept", "*/*").addHeader("Cache-Control", "no-cache").addHeader("Connection", "keep-alive")
+				.build();
 
 		try {
 			Response response = client.newCall(request).execute();
@@ -116,29 +114,29 @@ public class SweagleUtils {
 		} catch (IOException e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
-			else 
+			else
 				loggerUtils.error(e.toString());
 		}
-		
-		
+
 		return responseString;
 	}
 
-	static String snapshotConfig(String mdsName, String sweagleURL, Secret sweagleAPIkey, String description, String tag,  TaskListener listener) throws UnsupportedEncodingException {
+	static String snapshotConfig(String mdsName, String sweagleURL, Secret sweagleAPIkey, String description,
+			String tag, TaskListener listener) throws UnsupportedEncodingException {
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
 		loggerUtils.info("Creating Snapshot from pending data for " + mdsName);
 		String responseString = null;
-		
+
 		OkHttpClient client = new OkHttpClient();
 		Response response = null;
 		RequestBody body = RequestBody.create(null, new byte[0]);
 		Request request = new Request.Builder()
-		  .url(sweagleURL + "/api/v1/data/include/snapshot/byname?name="+mdsName+"&level=none&description="+URLEncoder.encode(description, "UTF-8").replace("+", "%20")+"&tag="+URLEncoder.encode(tag, "UTF-8").replace("+", "%20"))
-		  .post(body)
-		  .addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
-		  .addHeader("Accept", "*/*")
-		  .build();
+				.url(sweagleURL + "/api/v1/data/include/snapshot/byname?name=" + mdsName + "&level=none&description="
+						+ URLEncoder.encode(description, "UTF-8").replace("+", "%20") + "&tag="
+						+ URLEncoder.encode(tag, "UTF-8").replace("+", "%20"))
+				.post(body).addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
+				.addHeader("Accept", "*/*").build();
 
 		try {
 			response = client.newCall(request).execute();
@@ -148,29 +146,28 @@ public class SweagleUtils {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return responseString;
 	}
 
-	static String exportConfig(String sweagleURL, Secret sweagleAPIkey, String mdsName, String fileLocation, String exporter, String args, String format, boolean markFailed, TaskListener listener, EnvVars env) throws AbortException {
+	static String exportConfig(String sweagleURL, Secret sweagleAPIkey, String mdsName, String fileLocation,
+			String exporter, String args, String format, boolean markFailed, TaskListener listener, EnvVars env)
+			throws AbortException {
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
-		loggerUtils.info("Exporting from " + mdsName + " with exporter " + exporter+" in format "+format+" at "+ sweagleURL);
+		loggerUtils.info("Exporting from " + mdsName + " with exporter " + exporter + " in format " + format + " at "
+				+ sweagleURL);
 		String responseString = null;
 		OkHttpClient client = new OkHttpClient();
 		Response response = null;
 
 		MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-		RequestBody body = RequestBody.create(mediaType, "mds="+mdsName+"&parser="+exporter+"&args="+args+"&format="+format);
-		Request request = new Request.Builder()
-		  .url(sweagleURL + "/api/v1/tenant/metadata-parser/parse")
-		  .post(body)
-		  .addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
-		  .addHeader("Accept", "*/*")
-		  .addHeader("content-type", "application/x-www-form-urlencoded")
-		  .addHeader("Connection", "keep-alive")
-		  .addHeader("cache-control", "no-cache")
-		  .build();
+		RequestBody body = RequestBody.create(mediaType,
+				"mds=" + mdsName + "&parser=" + exporter + "&args=" + args + "&format=" + format);
+		Request request = new Request.Builder().url(sweagleURL + "/api/v1/tenant/metadata-parser/parse").post(body)
+				.addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey)).addHeader("Accept", "*/*")
+				.addHeader("content-type", "application/x-www-form-urlencoded").addHeader("Connection", "keep-alive")
+				.addHeader("cache-control", "no-cache").build();
 
 		try {
 			response = client.newCall(request).execute();
@@ -179,38 +176,35 @@ public class SweagleUtils {
 		} catch (Exception e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
-			else 
+			else
 				loggerUtils.error(e.toString());
 		}
-		
-		String content=responseString;
-		
+
+		String content = responseString;
+
 		try {
-			Files.write(Paths.get(env.get("WORKSPACE")+"/"+fileLocation), content.getBytes(StandardCharsets.UTF_8));
+			Files.write(Paths.get(env.get("WORKSPACE") + "/" + fileLocation), content.getBytes(StandardCharsets.UTF_8));
 		} catch (Exception e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
-			else 
+			else
 				loggerUtils.error(e.toString());
 		}
-		
-		
-		
+
 		return responseString;
 	}
-	
-	static boolean validateProgress(String mdsName, String sweagleURL, Secret sweagleAPIkey,  boolean markFailed, TaskListener listener) throws AbortException, InterruptedException  {
+
+	static boolean validateProgress(String mdsName, String sweagleURL, Secret sweagleAPIkey, boolean markFailed,
+			TaskListener listener) throws AbortException, InterruptedException {
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
 		String responseString = null;
 		OkHttpClient client = new OkHttpClient();
 		Response response = null;
 		Request request = new Request.Builder()
-				  .url(sweagleURL + "/api/v1/data/include/validation_progress?name=" + mdsName+ "&forIncoming=true")
-				  .get()
-				  .addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
-				  .addHeader("Accept", "*/*")
-				  .build();
+				.url(sweagleURL + "/api/v1/data/include/validation_progress?name=" + mdsName + "&forIncoming=true")
+				.get().addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey)).addHeader("Accept", "*/*")
+				.build();
 
 		try {
 			response = client.newCall(request).execute();
@@ -219,24 +213,22 @@ public class SweagleUtils {
 		} catch (Exception e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
-			else 
+			else
 				loggerUtils.error(e.toString());
 		}
-		
-		if (responseString != "Finished")
-		{
-			loggerUtils.info("Checking validation progress for " + mdsName + "status:" + responseString);
+
+		if (responseString.contains("FINISHED")) {
+			loggerUtils.info("Checking validation progress for " + mdsName + " validation complete ");
 			return true;
-			
+
+		} else {
+			loggerUtils.info("Checking validation progress for " + mdsName + " status:" + responseString);
+			return false;
 		}
-		loggerUtils.info("Checking validation progress for " + mdsName + "status:" + responseString);
-		return false;
-		}
-		
-	static String readFile(String path, Charset encoding) 
-			  throws IOException 
-			{
-			  byte[] encoded = Files.readAllBytes(Paths.get(path));
-			  return new String(encoded, encoding);
-			}
+	}
+
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
 }
