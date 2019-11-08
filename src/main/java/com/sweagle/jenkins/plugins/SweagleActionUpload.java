35eb318ddcf6798595c96597d13e738e14cb7037
Joe Offenberg <joe@sweagle.com>
2019-11-07 23:29:58 -0500

    Multi file upload with recursio

--- src/main/java/com/sweagle/jenkins/plugins/SweagleActionUpload.java
+++ src/main/java/com/sweagle/jenkins/plugins/SweagleActionUpload.java
@@ -24,14 +24,21 @@
 
 package com.sweagle.jenkins.plugins;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-
+import java.nio.charset.Charset;
 
 import javax.annotation.CheckForNull;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang.SystemUtils;
+import org.apache.tools.ant.DirectoryScanner;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
+import hudson.AbortException;
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.Launcher;
@@ -53,50 +60,57 @@ public class SweagleActionUpload extends hudson.tasks.Builder implements SimpleB
 
 	@CheckForNull
 	private String fileLocation;
-	
+
 	@CheckForNull
 	private String nodePath;
-	
+
 	@CheckForNull
 	private String format;
 	private boolean allowDelete = false;
 	private boolean onlyParent = false;
+	private boolean filenameNodes = false;
+	private boolean subDirectories = false;
 	private boolean withSnapshot = false;
 	private String tag;
 	private String description;
 	private boolean markFailed = false;
 	private boolean showResults = false;
-	
-
-	
-	
-
 
 	@DataBoundConstructor
-	public SweagleActionUpload(@CheckForNull String actionName, @CheckForNull String fileLocation, @CheckForNull String format, @CheckForNull String nodePath) {
+	public SweagleActionUpload(@CheckForNull String actionName, @CheckForNull String fileLocation,
+			@CheckForNull String format, @CheckForNull String nodePath) {
 		this.actionName = Util.fixEmptyAndTrim(actionName);
-		this.fileLocation=fileLocation;
-		this.nodePath=nodePath;
-		this.format=format;
-			
+		this.fileLocation = fileLocation;
+		this.nodePath = nodePath;
+		this.format = format;
+
 	}
 
 	@DataBoundSetter
 	public void setAllowDelete(boolean allowDelete) {
 		this.allowDelete = allowDelete;
 	}
-	
+
 	@DataBoundSetter
 	public void setOnlyParent(boolean onlyParent) {
 		this.onlyParent = onlyParent;
 	}
-	
+
+	@DataBoundSetter
+	public void setFilenameNodes(boolean filenameNodes) {
+		this.filenameNodes = filenameNodes;
+	}
+
+	@DataBoundSetter
+	public void setSubDirectories(boolean subDirectories) {
+		this.subDirectories = subDirectories;
+	}
+
 	@DataBoundSetter
 	public void setWithSnapshot(boolean withSnapshot) {
 		this.withSnapshot = withSnapshot;
 	}
 
-	
 	@DataBoundSetter
 	public void setMarkFailed(boolean markFailed) {
 		this.markFailed = markFailed;
@@ -106,56 +120,63 @@ public class SweagleActionUpload extends hudson.tasks.Builder implements SimpleB
 	public void setShowResults(boolean showResults) {
 		this.showResults = showResults;
 	}
-	
+
 	@DataBoundSetter
 	public void setTag(String tag) {
 		this.tag = tag;
 	}
-	
+
 	@DataBoundSetter
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
 	// getters
-	
+
 	public String getActionName() {
 		return actionName;
 	}
 
-
 	public String getFileLocation() {
 		return fileLocation;
 	}
-	
+
 	public String getNodePath() {
 		return nodePath;
 	}
-	
+
 	public String getFormat() {
 		return format;
 	}
-	
+
 	public boolean getAllowDelete() {
 		return allowDelete;
 	}
-	
+
 	public boolean getOnlyParent() {
 		return onlyParent;
 	}
-	
+
+	public boolean getFilenameNodes() {
+		return filenameNodes;
+	}
+
+	public boolean getSubDirectories() {
+		return subDirectories;
+	}
+
 	public boolean getWithSnapshot() {
 		return withSnapshot;
 	}
-	
+
 	public String getTag() {
 		return tag;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public boolean getMarkFailed() {
 		return markFailed;
 	}
@@ -167,17 +188,11 @@ public class SweagleActionUpload extends hudson.tasks.Builder implements SimpleB
 	public BuildStepMonitor getRequiredMonitorService() {
 		return BuildStepMonitor.NONE;
 	}
-	
-	
 
 	@Override
 	public DescriptorImpl_Upload getDescriptor() {
 		return (DescriptorImpl_Upload) super.getDescriptor();
 	}
-	
-	
-	
-	
 
 	// Job Plugin execution code
 	@Override
@@ -192,23 +207,67 @@ public class SweagleActionUpload extends hudson.tasks.Builder implements SimpleB
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
 		loggerUtils.info("Executing SWEAGLE Upload " + actionName + " at: " + sweagleURL);
-		
+
 		String fileLocationExp = env.expand(fileLocation);
 		String nodePathExp = env.expand(nodePath);
 		String descriptionEnv = env.expand(description);
 		String tagEnv = env.expand(tag);
-		
 		String actionResonse = null;
-		
-			
-		actionResonse = SweagleUtils.uploadConfig(sweagleURL, sweagleAPIkey, fileLocationExp,  nodePathExp, format, allowDelete, withSnapshot, onlyParent, tagEnv, descriptionEnv, markFailed, listener, showResults, env);
+
+		if (subDirectories) {
+			String separator = "/";
+			if (SystemUtils.IS_OS_WINDOWS)
+				separator = "\\";
+
+			String[] fileLocationExps = fileLocationExp.split(separator);
+			fileLocationExps[fileLocationExps.length - 1] = "**" + File.separator
+					+ fileLocationExps[fileLocationExps.length - 1];
+			fileLocationExp = Arrays.toString(fileLocationExps).replace("[", "").replace("]", "")
+					.replace(",", File.separator).replace(" ", "");
+		}
+		if (showResults)
+			loggerUtils.debug("DirectoryScanner setIncludes: " + fileLocationExp);
+
+		// Multi File Support
+
+		DirectoryScanner scanner = new DirectoryScanner();
+//		String directory = System.getProperty("user.dir");
+//		if (env.get("WORKSPACE") != null) {
+//			directory = env.get("WORKSPACE");
+		String directory = workspace.toString();
 
 		
-		
-		
-		if (showResults)
-		loggerUtils.debug(actionResonse);
-		
+		scanner.setBasedir(directory);
+		if (showResults) {
+			loggerUtils.debug("DirectoryScanner setBasedir: " + directory);
+		}
+		scanner.setIncludes(new String[] { fileLocationExp });
+		scanner.scan();
+		String[] files = scanner.getIncludedFiles();
+
+		loggerUtils.debug("DirectoryScanner found: " + files.length + " files.");
+		if (files.length > 0) {
+
+			int changeSetId = SweagleUtils.createChangeSet(sweagleURL, sweagleAPIkey, showResults, descriptionEnv,
+					listener, env);
+
+			for (String file : files) {
+				actionResonse = SweagleUtils.uploadConfig(sweagleURL, sweagleAPIkey, file, nodePathExp, format,
+						allowDelete, onlyParent, filenameNodes, markFailed, workspace, listener, showResults, changeSetId, env);
+			}
+
+			SweagleUtils.approveChangeSet(sweagleURL, sweagleAPIkey, tagEnv, descriptionEnv, withSnapshot, changeSetId,
+					showResults, listener, env);
+
+			if (showResults)
+				loggerUtils.debug(actionResonse);
+		} else {
+			if (markFailed) {
+				throw new AbortException("No matching files");
+			} else {
+				loggerUtils.info("No matching files");
+			}
+		}
 
 	}
 }
--- src/main/java/com/sweagle/jenkins/plugins/SweagleUtils.java
+++ src/main/java/com/sweagle/jenkins/plugins/SweagleUtils.java
@@ -1,5 +1,6 @@
 package com.sweagle.jenkins.plugins;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
@@ -8,14 +9,16 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Predicate;
+
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.util.Secret;
 import hudson.AbortException;
 import hudson.EnvVars;
+import hudson.FilePath;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -25,10 +28,7 @@ import okhttp3.Response;
 public class SweagleUtils {
 
 	static OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(60, TimeUnit.SECONDS)
-			.readTimeout(60, TimeUnit.SECONDS)
-			.writeTimeout(60, TimeUnit.SECONDS)
-			.build();
-	
+			.readTimeout(60, TimeUnit.SECONDS).writeTimeout(60, TimeUnit.SECONDS).build();
 
 	static String validateConfig(String mdsName, String sweagleURL, Secret sweagleAPIkey, boolean markFailed,
 			int warnMax, int errMax, TaskListener listener, boolean showResults, Run<?, ?> run)
@@ -76,42 +76,37 @@ public class SweagleUtils {
 
 	}
 
-	static String uploadConfig(String sweagleURL, Secret sweagleAPIkey, String fileLocation, String nodePath,
-			String format, boolean allowDelete, boolean withSnapshot, boolean onlyParent, String tag, String description, boolean markFailed,  TaskListener listener, boolean showResults, EnvVars env) throws AbortException, UnsupportedEncodingException {
+	static String uploadConfig(String sweagleURL, Secret sweagleAPIkey, String fileName, String nodePath, String format,
+			boolean allowDelete, boolean onlyParent, boolean filenameNodes, boolean markFailed, FilePath workspace, TaskListener listener,
+			boolean showResults, int changeset, EnvVars env) throws AbortException, UnsupportedEncodingException {
 		PrintStream logger = listener.getLogger();
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
-		loggerUtils.info("Uploading Config from " + fileLocation + " to " + nodePath);
+		loggerUtils.info("Uploading Config from " + fileName + " to " + nodePath);
 		String responseString = null;
 		String content = null;
+		String directory = ".";
+		directory = workspace.toString();
 		try {
-			if (env.get("WORKSPACE")!=null)
-			content = readFile(env.get("WORKSPACE") + "/" + fileLocation, Charset.defaultCharset());
-			else {
-			content = readFile(fileLocation, Charset.defaultCharset());
-			}
+			content = readFile(directory+File.separator+fileName, Charset.defaultCharset());
 		} catch (IOException e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
 			else
 				loggerUtils.error(e.toString());
 		}
-		
-		tag=Objects.toString(tag, "");
-		description=Objects.toString(description, "");
 
+		if (filenameNodes) {
+			nodePath = nodePath + "," + fileName;
+			nodePath = nodePath.replace("/", ",");
+			nodePath = nodePath.replace("\\", ",");
+		}
 
 		MediaType mediaType = MediaType.parse("text/plain");
 		RequestBody body = RequestBody.create(mediaType, content);
 		Request request = new Request.Builder()
-				.url(sweagleURL + "/api/v1/data/bulk-operations/dataLoader/upload?nodePath=" + nodePath 
-						+ "&format="+format
-						+ "&allowDelete="+allowDelete
-						+"&autoApprove=true&storeSnapshotResults="+withSnapshot
-						+"&validationLevel=error"
-						+"autoRetry=true"
-						+"&onlyParent="+onlyParent
-						+"&tag="+URLEncoder.encode(tag, "UTF-8").replace("+", "%20")
-						+"&description="+URLEncoder.encode(description, "UTF-8").replace("+", "%20"))
+				.url(sweagleURL + "/api/v1/data/bulk-operations/dataLoader/upload?nodePath=" + nodePath + "&format="
+						+ format + "&allowDelete=" + allowDelete + "&validationLevel=error" + "&changeset=" + changeset
+						+ "&autoRetry=true" + "&onlyParent=" + onlyParent)
 				.post(body).addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey))
 				.addHeader("Accept", "*/*").addHeader("Cache-Control", "no-cache").addHeader("Connection", "keep-alive")
 				.build();
@@ -120,12 +115,13 @@ public class SweagleUtils {
 			Response response = client.newCall(request).execute();
 			responseString = response.body().string();
 			if (showResults) {
-			loggerUtils.debug("request:" + request.toString());
-			loggerUtils.debug("response code: "+ response.code() + "  " + response.body());
-			loggerUtils.debug(response.toString());
+				loggerUtils.debug("request:" + request.toString());
+				loggerUtils.debug("response code: " + response.code() + "  " + response.body());
+				loggerUtils.debug(response.toString());
 			}
-			if (response.code() >299 && markFailed) {
-				throw new AbortException("Error " +response.code() + "  " +responseString);}
+			if (response.code() > 299 && markFailed) {
+				throw new AbortException("Error " + response.code() + "  " + responseString);
+			}
 			response.close();
 		} catch (IOException e) {
 			if (markFailed)
@@ -133,7 +129,7 @@ public class SweagleUtils {
 			else
 				loggerUtils.error(e.toString());
 		}
-		
+
 		return responseString;
 	}
 
@@ -143,7 +139,7 @@ public class SweagleUtils {
 		LoggerUtils loggerUtils = new LoggerUtils(logger);
 		loggerUtils.info("Creating Snapshot from pending data for " + mdsName);
 		String responseString = null;
-		
+
 		Response response = null;
 		RequestBody body = RequestBody.create(null, new byte[0]);
 		Request request = new Request.Builder()
@@ -156,8 +152,9 @@ public class SweagleUtils {
 		try {
 			response = client.newCall(request).execute();
 			responseString = response.body().string();
-			if (response.code() >299 && markFailed) {
-				throw new AbortException("Error " +response.code() + "  " +responseString);}
+			if (response.code() > 299 && markFailed) {
+				throw new AbortException("Error " + response.code() + "  " + responseString);
+			}
 			response.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -188,8 +185,9 @@ public class SweagleUtils {
 		try {
 			response = client.newCall(request).execute();
 			responseString = response.body().string();
-			if (response.code() >299 && markFailed) {
-				throw new AbortException("Error " +response.code() + "  " +responseString);}
+			if (response.code() > 299 && markFailed) {
+				throw new AbortException("Error " + response.code() + "  " + responseString);
+			}
 			response.close();
 		} catch (Exception e) {
 			if (markFailed)
@@ -201,11 +199,12 @@ public class SweagleUtils {
 		String content = responseString;
 
 		try {
-			if (env.get("WORKSPACE")!=null)
-			Files.write(Paths.get(env.get("WORKSPACE") + "/" + fileLocation), content.getBytes(StandardCharsets.UTF_8));
-			else 
-			Files.write(Paths.get(fileLocation), content.getBytes(StandardCharsets.UTF_8));
-			
+			if (env.get("WORKSPACE") != null)
+				Files.write(Paths.get(env.get("WORKSPACE") + "/" + fileLocation),
+						content.getBytes(StandardCharsets.UTF_8));
+			else
+				Files.write(Paths.get(fileLocation), content.getBytes(StandardCharsets.UTF_8));
+
 		} catch (Exception e) {
 			if (markFailed)
 				throw new AbortException(e.toString());
@@ -252,19 +251,85 @@ public class SweagleUtils {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return new String(encoded, encoding);
 	}
-	
+
 	static String getErrorfromResponse(String responseAsString) {
-		String errorMessage="";
+		String errorMessage = "";
 		if (responseAsString.isEmpty()) {
-		return "";	
+			return "";
 		} else {
-		try {	
-		errorMessage = JsonPath.read(responseAsString, "$.error");
-		} catch (Exception e) {
-		errorMessage = "Not a valid response from SWEAGLE API "  + responseAsString;
-		}
-		
-		return errorMessage;
+			try {
+				errorMessage = JsonPath.read(responseAsString, "$.error");
+			} catch (Exception e) {
+				errorMessage = "Not a valid response from SWEAGLE API " + responseAsString;
+			}
+
+			return errorMessage;
 		}
 	}
+
+	public static int createChangeSet(String sweagleURL, Secret sweagleAPIkey, boolean showResults, String description,
+			TaskListener listener, EnvVars env) throws IOException, InterruptedException {
+
+		PrintStream logger = listener.getLogger();
+		LoggerUtils loggerUtils = new LoggerUtils(logger);
+		String title = null;
+
+		try {
+			title = URLEncoder.encode("Jenkins Upload Changeset ", "UTF-8");
+			if (description == null || description.equals("") || description.isEmpty())
+				description = URLEncoder.encode(env.get("JOB_NAME") + "Build Number: " + env.get("BUILD_NUMBER"),
+						"UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		final RequestBody requestBody = RequestBody.create((MediaType) null, "");
+		final Request createChangeSet = new Request.Builder()
+				.url(sweagleURL + "/api/v1/data/changeset?title=" + title + "&description=" + description)
+				.post(requestBody).addHeader("Accept", "*/*")
+				.addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey)).build();
+		String createChangeStResponseString = null;
+		try {
+			final Response createChangeStResponse = client.newCall(createChangeSet).execute();
+			createChangeStResponseString = createChangeStResponse.body().string();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+
+		if (showResults)
+			loggerUtils.debug("create changeset: " + createChangeStResponseString);
+
+		final int changeSetId = (int) JsonPath.read(createChangeStResponseString, "$.id", new Predicate[0]);
+		return changeSetId;
+	}
+
+	public static void approveChangeSet(String sweagleURL, Secret sweagleAPIkey, String tag, String description,
+			boolean withSnapshot, int changeSetId, boolean showResults, TaskListener listener, EnvVars env)
+			throws UnsupportedEncodingException {
+
+		if (description == null || description.equals("") || description.isEmpty()) {
+			description = "Jenkins Job: " + env.get("JOB_NAME") + "  Build Number: " + env.get("BUILD_NUMBER");
+		}
+		PrintStream logger = listener.getLogger();
+		LoggerUtils loggerUtils = new LoggerUtils(logger);
+		final RequestBody requestBody = RequestBody.create((MediaType) null, "");
+		final Request approveChangeSet = new Request.Builder()
+				.url(sweagleURL + "/api/v1/data/changeset/" + changeSetId + "/approve" + "?tag="
+						+ URLEncoder.encode(tag, "UTF-8").replace("+", "%20") + "&description="
+						+ URLEncoder.encode(description, "UTF-8").replace("+", "%20") + "&snapshotDescription="
+						+ URLEncoder.encode(description, "UTF-8").replace("+", "%20") + "&withSnapshot=" + withSnapshot)
+				.post(requestBody).addHeader("Accept", "*/*")
+				.addHeader("Authorization", "Bearer " + Secret.toString(sweagleAPIkey)).build();
+		String approveChangeStResponseString = null;
+		try {
+			final Response approveChangeStResponse = client.newCall(approveChangeSet).execute();
+			approveChangeStResponseString = approveChangeStResponse.body().string();
+
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+		if (showResults)
+			loggerUtils.debug("approve changeset: " + approveChangeStResponseString);
+
+	}
+
 }
